ae5eb43fe58b19d00dc37f3944ab0763f0bed205
yo1000 <yoichi.kikuchi.aug@gmail.com>
2017-05-25 00:13:50 +0900

    Migrate models language to Kotlin

--- src/main/kotlin/red/sukun1899/shishamo/model/CreateTableStatement.kt
+++ src/main/kotlin/red/sukun1899/shishamo/model/CreateTableStatement.kt
@@ -3,23 +3,7 @@ package red.sukun1899.shishamo.model;
 /**
  * @author su-kun1899
  */
-public class CreateTableStatement {
-    private String tableName;
-    private String ddl;
-
-    public String getTableName() {
-        return tableName;
-    }
-
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
-    }
-
-    public String getDdl() {
-        return ddl;
-    }
-
-    public void setDdl(String ddl) {
-        this.ddl = ddl;
-    }
-}
+data class CreateTableStatement(
+        val tableName: String,
+        val ddl: String
+)
--- src/main/kotlin/red/sukun1899/shishamo/model/Index.kt
+++ src/main/kotlin/red/sukun1899/shishamo/model/Index.kt
@@ -1,51 +1,21 @@
 package red.sukun1899.shishamo.model;
 
-import java.util.List;
-
 /**
  * @author su-kun1899
  */
-public class Index {
-    private String name;
-    private List<Column> columns;
-    private Category category;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Column> getColumns() {
-        return columns;
-    }
-
-    public void setColumns(List<Column> columns) {
-        this.columns = columns;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public enum Category {
+data class Index(
+        val name: String,
+        val columns: List<Column>,
+        val category: Category
+) {
+    enum class Category(
+            private val order: Int
+    ) {
         PRIMARY(10),
         UNIQUE(20),
         PERFORMANCE(30);
 
-        private int order;
-
-        Category(int order) {
-            this.order = order;
-        }
-
-        public int order() {
+        fun order(): Int {
             return order;
         }
     }
