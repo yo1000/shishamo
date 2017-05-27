f2e827ed75f207e247d2b463b5853552e679b627
yo1000 <yoichi.kikuchi.aug@gmail.com>
2017-05-28 02:45:00 +0900

    Remove useless annotations

--- src/main/kotlin/red/sukun1899/shishamo/repository/TableRepository.kt
+++ src/main/kotlin/red/sukun1899/shishamo/repository/TableRepository.kt
@@ -1,7 +1,5 @@
 package red.sukun1899.shishamo.repository;
 
-import org.apache.ibatis.annotations.MapKey
-import org.apache.ibatis.annotations.Param
 import red.sukun1899.shishamo.model.CreateTableStatement
 import red.sukun1899.shishamo.model.ReferencedTableCount
 import red.sukun1899.shishamo.model.Table
@@ -11,17 +9,9 @@ import red.sukun1899.shishamo.model.Table
  */
 interface TableRepository {
     fun selectAll(schemaName: String): List<Table>
-
-    fun select(@Param("schemaName") schemaName: String, @Param("name") name: String): Table
-
-    @MapKey("baseTableName")
+    fun select(schemaName: String, name: String): Table
     fun selectParentTableCountsByTableName(schemaName: String): Map<String, ReferencedTableCount>
-
-    @MapKey("baseTableName")
     fun selectChildTableCountsByTableName(schemaName: String): Map<String, ReferencedTableCount>
-
-    @MapKey("baseTableName")
     fun selectColumnCountsByTableName(schemaName: String): Map<String, ReferencedTableCount>
-
     fun showCreateTableStatement(table: Table): CreateTableStatement
 }
